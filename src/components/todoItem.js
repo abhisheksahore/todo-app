8a623b9510f3e2f045506d3d8ea310c42b5517d4
abhisheksahore <abhisheksahore7@gmail.com>
2021-04-05 23:12:28 +0530

    styles the completed tasks

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -7,8 +7,8 @@ class TodoItem extends React.Component {
         return (
             <div className="list-item">
                 <label>
-                    <input id={this.props.id} type="checkbox" checked={this.props.checked} onClick={(e)=>this.props.updateChecked(this.props.id)}/>
-                    <span>{this.props.itemText}</span>
+                    <input id={this.props.id} type="checkbox" checked={this.props.isChecked} onClick={(e)=>this.props.updateChecked(this.props.id)}/>
+                    <span style={this.props.isChecked? {color: 'grey', textDecoration: 'line-through'}: null}>{this.props.itemText}</span>
                 </label>
                 <DeleteBtn deleteItem={this.props.deleteItem} id={this.props.id}/>
             </div>
